e92221e71aaef0d02e4f65c570d02d5e2582c882
AltheaJT <jindumorsindi@gmail.com>
2026-02-20 15:27:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const app = document.getElementById("main-container");
+const temp = document.getElementById("temp-txt");
+const dateOutput = document.querySelector(".current-date-txt");
+const timeOutput = document.querySelector(".current-time-txt");
+const conditionOutput = document.querySelector(".condition-txt");
+const countryOutput = document.querySelector(".country-txt");
+const icon = document.querySelector(".weather-summary-img");
+const humidityOutput = document.querySelector(".humidity-info");
+const windOutput = document.querySelector(".wind-info");
+
+const form = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+const liveBtn = document.getElementById("live-btn");
+
